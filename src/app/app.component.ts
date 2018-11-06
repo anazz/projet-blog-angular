adeed2753c91fe8c6f38afcf61aba6850debc710
Ana Zegheanu <ana.zegheanu@gmail.com>
2018-11-06 14:43:51 +0100

    added all elements for the project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'projet-blog';
+
+  posts = [
+  	{
+  		title: 'Mon premier post',
+  		content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat',
+  		loveIts: 10,
+  		created_at: new Date()
+  	},
+  	{
+  		title: 'Mon deuxième post',
+  		content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat',
+  		loveIts: -16,
+  		created_at: new Date()
+  	},
+  	{
+  		title: 'Encore un post',
+  		content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat',
+  		loveIts: 0,
+  		created_at: new Date()
+  	}
+  ];
 }
--- src/app/post-list-item/post-list-item.component.ts
+++ src/app/post-list-item/post-list-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-post-list-item',
@@ -7,9 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostListItemComponent implements OnInit {
 
+  @Input() postTitle: string;
+  @Input() postContent: string;
+  @Input() postLoveIts: number;
+  @Input() postCreated_at: Date;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onIncrement(){
+     this.postLoveIts += 1;
+ 	}
+ 
+  onDecrement(){
+     this.postLoveIts -= 1;
+  }
+
+  getPostLoveIts(){
+  	return this.postLoveIts;
+  }
+
+  getColor() {
+ 
+    if(this.getPostLoveIts() < 0){
+         return 'red';
+    }else if(this.getPostLoveIts() > 0){
+         return 'green';
+    }
   }
 
 }
